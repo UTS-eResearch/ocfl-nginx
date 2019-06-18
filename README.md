da9266593c59fccda38e16c594be741c5015d76a
Mike Lynch <mike@mikelynch.org>
2019-06-19 09:20:55 +1000

    ocfl remapping now preserves content paths

--- README.md
+++ README.md
@@ -8,6 +8,19 @@ JavaScript utilities for serving OCFL repositories via nginx.
 - ocfl.conf - nginx config file
 - ocfl.js - javascript to map incoming URLs to ocfl object paths
 
+## Roadmap
+
+### UTS Stash MVP
+
+Resolving oids to OCFL paths to serve datasets as part of the UTS reseach data catalogue toolchain.
+
+### Versions
+
+Static versions - write out copies or symlinks of each version
+
+Explore feasibility of dynamic versions, which would do internal nginx redirects to the latest version of a file based on the manifest
+
+Expose versions to the web using a protocol such as memento
 
 
 
--- js/ocfl.js
+++ js/ocfl.js
@@ -1,37 +1,37 @@
 // ocfl utilities
 
+// ocfl(request)
+//
+// entry-point from nginx
+//
+// expects two parameters set on the request variables:
+// ocfl_path - the part of the URL path before the OID in the incoming URL
+// ocfl_repo - the nginx location mapped to the actual ocfl file root
+
+function ocfl(req) {
+    var url_path = req.variables.ocfl_path;
+    var ocfl_repo = req.variables.ocfl_repo;
+    var pattern = new RegExp(url_path + '/([^/]+)(.*)$');
+    var match = req.uri.match(pattern);
+    if( !match ) {
+        req.error("Match failed " + pattern);
+        req.return(500, "ocfl - url match failed");
+    } else {
+        var oid = match[1];
+        var content = match[2];
+        var object = oid_pairtree(oid);
+        var opath = [ ocfl_repo ].concat(object).join('/');
+        var newroute = '/' + opath + '/v1/content/' + content;
+        req.warn("Remapped " + oid + " to " + newroute);
+        req.internalRedirect(newroute);
+    }
+}
 
 // convert an oid to an array representing a path in the
 // ocfl repository (the pairtree)
 
+// TODO: replace this bad regexp version with something more robust
+
 function oid_pairtree(oid) {
     return oid.match(/.{1,2}/g);
 }
-
-
-
-
-
-
-// entry-point from nginx
-
-// note: staging_js -> staging
-
-function ocfl_resolve(req) {
-    try {
-        var parts = req.uri.split('/');
-        var oid = parts[parts.length - 1];
-        var ocfl_path = oid_to_ocfl(oid);
-        var route = [ 'staging' ];
-        var newroute = '/' + route.concat(ocfl_path).join('/') + '/v1/content/';
-        req.warn("Remapped " + oid + " to " + newroute);
-        req.internalRedirect(newroute);
-    } catch(e) {
-        req.error("ocfl_resolve error: " + e);
-        req.return(500, "ocfl_resolve: " + e);
-    }
-}
-
-
-
-
