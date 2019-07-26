7b148942eb4eaf52ff506e9a581e2a01fa028e6c
Mike Lynch <mike@mikelynch.org>
2019-07-26 14:37:14 +1000

    Removed old non-versioned ocfl handler

--- js/ocfl.js
+++ js/ocfl.js
@@ -6,36 +6,9 @@ var fs = require('fs');
 // ocfl(request)
 //
 // entry-point from nginx
-//
-// expects two parameters set on the request variables:
-// ocfl_path - the part of the URL path before the OID in the incoming URL
-// ocfl_repo - the nginx location mapped to the actual ocfl file root
 
 
 function ocfl(req) {
-  var url_path = req.variables.ocfl_path;
-  var ocfl_repo = req.variables.ocfl_repo;
-  var index_file = req.variables.ocfl_index_file;
-  var pattern = new RegExp(url_path + '/([^/]+)/(.*)$');
-  var match = req.uri.match(pattern);
-  if( !match ) {
-    req.error("Match failed " + pattern);
-    req.return(500, "ocfl - url match failed");
-  } else {
-    var oid = match[1];
-    var content = match[2] || index_file;
-    var object = pairtree(oid);
-    var opath = [ ocfl_repo ].concat(object).join('/');
-    var newroute = '/' + opath + '/v1/content/' + content;
-    req.warn("Remapped " + oid + " to " + newroute);
-    req.internalRedirect(newroute);
-  }
-}
-
-
-// version with versions
-
-function ocfl_versioned(req) {
   var url_path = req.variables.ocfl_path;
   var ocfl_repo = req.variables.ocfl_repo;
   var ocfl_files = req.variables.ocfl_files;
